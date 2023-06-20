4f00be10f83f616abb976831dc4f2f9192abbd4d
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-06-20 23:22:22 +0530

    object creation, constructor, public method

--- Hello.java
+++ Hello.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("hello world");
+
+        int num1;
+        String name;
+        System.out.println("Enter the number");
+        num1 = sc.nextInt();
+        sc.nextLine();  // Clear the newline character from the input buffer
+        System.out.println("Enter the name");
+        name = sc.nextLine();
+        Display di = new Display();
+        di.display(num1, name);
     }
 }
