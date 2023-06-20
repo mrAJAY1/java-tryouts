501951722deae6af59d4d0d641af2eaad6dcb02d
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-06-20 23:39:07 +0530

    basic calculation

---
+++ Calculation.java
@@ -0,0 +1,12 @@
+public class Calculation {
+
+    public void sum(int a, int b) {
+        System.out.println("The sum is : " + (a + b));
+    }
+
+    public void difference(int a, int b) {
+        int difference;
+        difference = a - b;
+        System.out.println("The difference is: " + Math.abs(difference));
+    }
+}
--- Hello.java
+++ Hello.java
@@ -5,14 +5,33 @@ public class Hello {
         Scanner sc = new Scanner(System.in);
         System.out.println("hello world");
 
-        int num1;
+        int number, num1, num2, choice;
         String name;
         System.out.println("Enter the number");
-        num1 = sc.nextInt();
-        sc.nextLine();  // Clear the newline character from the input buffer
+        number = sc.nextInt();
+        sc.nextLine(); // Clear the newline character from the input buffer
         System.out.println("Enter the name");
         name = sc.nextLine();
         Display di = new Display();
-        di.display(num1, name);
+        di.display(number, name);
+        System.out.println("Next, choose an option: 1 - sum, 2 - difference");
+        choice = sc.nextInt();
+        System.out.println("Enter the numbers");
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        Calculation calc = new Calculation();
+        switch (choice) {
+            case 1: {
+                calc.sum(num1, num2);
+                break;
+            }
+            case 2: {
+                calc.difference(num1, num2);
+                break;
+            }
+            default: {
+                System.out.println("Invalid selection");
+            }
+        }
     }
 }
