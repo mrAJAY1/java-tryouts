98ba15baee01f55801469577db5fe11921965f7c
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-06-20 23:23:34 +0530

    object creation

---
+++ Display.java
@@ -0,0 +1,8 @@
+public class Display {
+    Display() {
+        System.out.println("Display object created");
+    }
+    public void display(int a, String b) {
+        System.out.println("Name " + b + "," + "number " + a);
+    }
+}
